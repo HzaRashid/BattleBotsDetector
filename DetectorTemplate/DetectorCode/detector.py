5731c36baf47a7abbdd25dac28ec495bb6097d2b
HzaRashid <hamzarashid.mail@gmail.com>
2025-01-30 18:18:31 -0500

    misc

--- DetectorTemplate/DetectorCode/detector.py
+++ DetectorTemplate/DetectorCode/detector.py
@@ -1,5 +1,7 @@
 from abc_classes import ADetector
 from teams_classes import DetectionMark
+import random
+
 
 class Detector(ADetector):
     def detect_bot(self, session_data):
@@ -8,7 +10,10 @@ class Detector(ADetector):
         marked_account = []
         
         for user in session_data.users:
-            marked_account.append(DetectionMark(user_id=user['id'], confidence=50, bot=False))
+            prediction = random.choice((True, False))
+            # print(prediction)
+            
+            marked_account.append(DetectionMark(user_id=user['id'], confidence=50, bot=prediction))
 
         return marked_account
     
