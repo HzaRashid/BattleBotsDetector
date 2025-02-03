8341ddd7165a571001523722ab4ca4f7ffd7ca7a
HzaRashid <hamzarashid.mail@gmail.com>
2025-02-02 22:30:06 -0500

    misc

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Run script
 python3 DetectorTemplate/main_detector.py
 
-if [[ "$(whoami)" != "hamzarashid" ]]
- then python3 email_results.py
- else echo "testing done"
-fi
+# if [[ "$(whoami)" != "hamzarashid" ]]
+python3 email_results.py
+#  else echo "testing done"
+# fi
